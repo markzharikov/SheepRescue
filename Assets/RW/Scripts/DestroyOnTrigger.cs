c14814dd864e2e7bb1a80bc3b9e6f207797a4229
markzharikov <104099507+markzharikov@users.noreply.github.com>
2022-05-05 23:03:44 +0200

    d

---
+++ Assets/RW/Scripts/DestroyOnTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnTrigger : MonoBehaviour
+{
+    public string tagFilter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(tagFilter))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
